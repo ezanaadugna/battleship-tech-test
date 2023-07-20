ce52b363d740503a27eb0c4bfa872f90e92da060
Ezana Adugna <eadugna3005@gmail.com>
2023-07-20 11:36:25 +0100

    pseudo program for ship.js

---
+++ src/Ship.js
@@ -0,0 +1,15 @@
+//Properties
+
+// size: The size of the ship
+// position: the position of the ship on the board. represented as an obkect with x and y co-ordinates.
+// direction: The direction the ship is facing. represented as a string ("vertical", "horizontal").
+
+
+class Ship {
+  constructor(size){
+    // Initializes the ship with the given size.
+  } 
+  hit(position) { 
+    // Accepts a position and checks if the ship is hit at that location. If it is hit, update the ship's internal state to reflect the hit.
+  }
+}
